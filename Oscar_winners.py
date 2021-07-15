b05e171e31d95aa71d5c37de51dc011e9849ed23
7Dany6 <dany.sulimov@yandex.ru>
2021-07-15 22:06:38 +0300

    First commit

---
+++ Oscar_winners.py
@@ -0,0 +1,7 @@
+"""
+The first Oscar winners got their prizes in 1929.
+We have data concerning 100 women and 100 men as well.
+We are going to use t-test in order to find out whether differences between age of women and men statistically reliable.
+Average age of men is 45, standart deviation = 9
+Average age of women if 34, standart deviation = 10
+"""
